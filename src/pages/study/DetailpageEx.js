0676c16f73a136ee0b84f37d7ffad2e20c871568
hdsjiw <105874382+hdsjiw@users.noreply.github.com>
2024-04-10 12:38:28 +0900

    Update DetailpageEx.js

--- src/pages/study/DetailpageEx.js
+++ src/pages/study/DetailpageEx.js
@@ -1,19 +1,54 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import styled from "styled-components";
 
+const DetailPageEx = ({title,date,content,tag}) => {
+    const { postId } = useParams();
 
-// import DetailPage from "./Detailpage";
-
-// const DetailPageEx = () => {
-//      const [title,date,tag]={
-//          title : '소프트웨어 공학_5주차_필기본',
-//          date: '2024-03-28',
-//         tag : '#소공 #소공5주차 #좋아요 #좋반'
-//        };
-//     return(
-//         <DetailPage title='소공' date='0328' tag='어저구' />
-        
-//     )
+    return(
+    <>
+       <h1><Title>{title}</Title></h1>
+       <p><Date>{date}</Date></p>
+       <p><PostId>Posted By &nbsp; Greek{postId}</PostId></p>
+      
+       <br />
+       {/* Content : 글, 그림 등등등*/}
+       <p><Content>{content}</Content></p>
+       <img src={process.env.PUBLIC_URL + "/example.png"} width="200px" height="200px"/>
+       <p><Tag>{tag}</Tag></p>
+      
+      </> 
+    )
 
     
-// }
+}
 
-// export default DetailPageEx;
+export default DetailPageEx;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  text:bold;
+  text-align:center;
+`;
+
+const Date = styled.p`
+  font-size: 1rem;
+  text-align:right;
+`;
+
+const PostId = styled.p`
+  font-size: 1.2rem;
+  text:bold
+`;
+
+const Tag = styled.p`
+  font-size: 1rem;
+  text:bold;
+  color: blue;
+  margin-top:12rem;
+`;
+
+const Content = styled.p`
+  font-size: 1rem;
+  text:bold
+`;
